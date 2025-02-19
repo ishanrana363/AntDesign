66f0bb273918220f4065566a746c56fd777acb1d
ishanrana363 <ishanrana363@gmail.com>
2025-02-19 21:08:31 +0600

    icon added

--- antd-demo/src/app/page.tsx
+++ antd-demo/src/app/page.tsx
@@ -1,9 +1,13 @@
-import AntButton from '@/components/button/AntButton';
+// import AntButton from '@/components/button/AntButton';
 import React from 'react';
+import AntIcon from './../components/icons/AntIcon';
 const page = () => {
   return (
     <div>
-      <AntButton></AntButton>
+      {/* <AntButton></AntButton> */}
+      {/* <FlotButton></FlotButton> */}
+      <AntIcon/>
+    
     </div>
   );
 };
---
+++ antd-demo/src/components/icons/AntIcon.tsx
@@ -0,0 +1,12 @@
+import { StepBackwardOutlined } from '@ant-design/icons'
+import React from 'react'
+
+const AntIcon = () => {
+  return (
+    <div>
+      <StepBackwardOutlined />
+    </div>
+  )
+}
+
+export default AntIcon
